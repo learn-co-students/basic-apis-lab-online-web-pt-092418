3d030075bb22401e0ed30d89a8277fccfa5a8530
github username <github email address>
2019-05-03 22:31:32 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,9 +1,25 @@
 class RepositoriesController < ApplicationController
 
   def search
-
   end
+
+
+  # baseURL = 'https://api.github.com'
+  # search = '/search/repositories'
 
   def github_search
+     # binding.pry
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+      req.params['client_id'] = 'hide it from share'
+      req.params['client_secret'] = 'hide it from share'
+      req.params['q'] = params[:query]
+    end
+
+    @body = JSON.parse(@resp.body)
+    @items = @body['items']
+
+    render 'search'
   end
-end
+
+
+end #end of controller
